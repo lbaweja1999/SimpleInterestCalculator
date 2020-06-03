99ffc6e7a2c8513a29c3419a8dcb8574ae17716d
Lakshay Baweja <lakshay17csu093@ncuindia.edu>
2020-06-03 22:37:28 +0530

    Debug Banner removed

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import './screens/SIcalc.dart';
 void main() {
   runApp(new MaterialApp(
     theme: ThemeData.dark(),
+    debugShowCheckedModeBanner: false,
     title: "SI Calculator",
     home: new SIcalc(),
   ));
